86bddd3b2c8829a8205401b89594c101f84bd356
余生爱静 <yutao59@hotmail.com>
2022-11-06 23:43:53 +0800

    增加MethodChannel的内容

--- android/app/src/main/kotlin/com/example/flutter_hello/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_hello/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterView
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.BasicMessageChannel
 import io.flutter.plugin.common.StringCodec
---
+++ lib/channel/method_channel.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class FlutterMethodChannel extends StatelessWidget{
+                MethodChannel methodChannel=MethodChannel('nativeCall');
+  @override
+  Widget build(BuildContext context) {
+        return MaterialApp(
+          home: Scaffold(
+            body: GestureDetector(
+              onTap: (){
+                _callNativeMethod();
+              },
+              child: Center(
+                child: Text('调用Native方法'),
+              ),
+            ),
+          ),
+        );
+  }
+
+  void _callNativeMethod() async{
+    try {
+      methodChannel.invokeMethod('nativeMethod');
+    } catch (e) {
+      print(e);
+    }
+  }
+}
